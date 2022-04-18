0416707f770e5ef6877e3e82312060c4727b9261
spbproger <zemli2016@yandex.ru>
2022-04-19 02:04:34 +0300

    Working app

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ from flask import Flask
 from utils import candidates_info, candidates_output, candidate_info_by_id, candidate_info_by_skills
 
 
-# Затем создадим экземпляр этого Flask, назовем его app -
-# это будет наше приложение
-
-
 app = Flask(__name__)
 
-@app.route("/")
+# Опишем декораторы
+
+
+@app.route("/")     # Основная страница
 def main():
     candidates_info_list = candidates_info()
     return candidates_output(candidates_info_list)
 
 
-@app.route("/candidates/<int:candidate_id>/")
+@app.route("/candidates/<int:candidate_id>/")       # Страница со сведениями о кандидате
 def candidates(candidate_id):
     candidates_info_list = candidates_info()
     candidate = candidate_info_by_id(candidates_info_list, candidate_id)
@@ -24,8 +23,7 @@ def candidates(candidate_id):
 
 
 
-
-@app.route("/skills/<skill>/")
+@app.route("/skills/<skill>/")      # Страница со сведениями о навыках андидатов
 def skills(skill):
     candidates_info_list = candidates_info()
     candidate = candidate_info_by_skills(candidates_info_list, skill)
--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
 import json
 
 def candidates_info(json_='candidates.json'):
+    """
+    Загружает сведения о кандидатах в json-формате
+    """
     with open(json_, 'r', encoding='utf-8') as f:
         return json.load(f)
 
 
 def candidates_output(candidates_info_list):
+    """
+    Выводит сведения о кандидатах в формате: имя, позиция, навыки
+    """
     candidates = '<pre>'
     for candidate in candidates_info_list:
         candidates += (f'Имя кандидата - {candidate["name"]}\n'
@@ -16,12 +22,18 @@ def candidates_output(candidates_info_list):
 
 
 def candidate_info_by_id(candidates_info_list, candidate_id):
+    """
+    Выводит сведения о кандидате по порядковому номеру (id) в списке
+    """
     for candidate in candidates_info_list:
         if candidate['id'] == candidate_id:
             return candidate
 
 
 def candidate_info_by_skills(candidates_info_list, skill):
+    """
+    Выводит сведения о кандидате по его навыкам
+    """
     candidates = '<pre>'
     for candidate in candidates_info_list:
         if skill.lower() in candidate['skills'].lower():
